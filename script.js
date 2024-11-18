bf13553f0b4dfdf5a0d3bef74c7de2697721f9d6
Gustavo Martins dos Santos <107081824+Finl1n@users.noreply.github.com>
2024-11-18 18:57:04 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+function toggleMenu() {
+  const navLinks = document.querySelector('.nav-links');
+  navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
+}
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.service-card');
+const totalSlides = slides.length;
+
+function moveSlide(direction) {
+  currentSlide += direction;
+  if (currentSlide < 0) {
+    currentSlide = totalSlides - 1;
+  } else if (currentSlide >= totalSlides) {
+    currentSlide = 0;
+  }
+  updateCarousel();
+}
+
+function updateCarousel() {
+  const track = document.querySelector('.carousel-track');
+  track.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+let slideInterval;
+
+function startAutoSlide() {
+  slideInterval = setInterval(() => moveSlide(1), 3000);
+}
+
+function stopAutoSlide() {
+  if (slideInterval) {
+    clearInterval(slideInterval);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const isMobile = window.matchMedia("(max-width: 768px)").matches;
+  
+  if (isMobile) {
+    const carousel = document.querySelector('.carousel-track');
+    carousel.classList.add('static-carousel');
+    stopAutoSlide();
+  }
+});
+
+startAutoSlide();
